56ce365e18f8eb3ad6759b239098fa3eba187a28
echanr <90714775+echanr@users.noreply.github.com>
2021-09-14 21:21:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 function compute() {
     //Get the values and calculate 
-   var principal = document.getElementById("principal").value;
-   var rate = document.getElementById("rate").value;
-   var years = document.getElementById("years").value;
-   var interest = principal * years * rate /100;
-   var year = new Date().getFullYear()+parseInt(years);
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + years;
     //Create the Interest text
     document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
         "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
         "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
-        "in the year <mark>" + Date + "</mark>";
+        "in the year <mark>" + yearInTheFuture + "</mark>";
 
 }
 
-//update the rate value
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+//update ther ate value
+function getSliderValue() {
+    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
 }
-
 
 //Check for positive values
 function validateAmount() {
